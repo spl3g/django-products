ca2eccf4bd58b2618149f8f33c797c5130eda5c9
spl3g <spleefer6@yandex.ru>
2024-06-17 13:32:23 +0300

    fix tests

--- products/settings.py
+++ products/settings.py
@@ -80,25 +80,14 @@ WSGI_APPLICATION = "products.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-if not getenv("DEBUG"):
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": getenv("PG_DBNAME"),
-            "USER": getenv("PG_USER"),
-            "PASSWORD": getenv("PG_PASSWORD"),
-            "HOST": getenv("PG_HOST"),
-            "PORT": getenv("PG_PORT"),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": "psrdb",
-            "HOST": "localhost",
-        }
-    }
+}
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
